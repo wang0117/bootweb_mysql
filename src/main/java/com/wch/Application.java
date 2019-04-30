a8b1a5d5bde3fdbb490268cab02cbca1acf759ae
pc-PC\pc <wangchenghong@persagy.com>
2019-04-30 11:51:27 +0800

    add package code

--- src/main/java/com/wch/Application.java
+++ src/main/java/com/wch/Application.java
@@ -2,6 +2,8 @@ package com.wch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Description:
@@ -10,8 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since: 2019年4月29日: 上午10:01:52
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
